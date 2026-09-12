f5e0a92991fad8d47aa2446853bda1ced289e9a5
TheRealCiggyWiggy <cygnus.wiebe@gmail.com>
2026-09-12 12:16:31 -0400

    Update config.php

--- php/php/main/config.php
+++ php/php/main/config.php
@@ -2,7 +2,7 @@
 
 /**
  * File: config.php
- * Author: Cygnus J. Wiebe
+ * Author: C. J. Wiebe
  * Date: April 2026
  * Project: Skyrim Character Combat Optimization System
  * Course: INFS 657: Data Design and Development - McGill University
@@ -14,4 +14,3 @@
 
 // Base URL for the project
 define('BASE_URL', '/infs_657/skyrim_inventory_optimization/website');
-?>
